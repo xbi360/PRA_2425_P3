bf3cd85f58a54382046cf5da2db2a3f216d79eb7
sunday <llaesasunday@gmail.com>
2024-12-12 06:31:35 +0100

    Añadida operator

--- TableEntry.h
+++ TableEntry.h
@@ -18,6 +18,13 @@ class TableEntry {
 		friend bool operator!=(const TableEntry<V> &te1, const TableEntry<V> &te2){
 			return te1.key != te2.key;
 		}
+		
+		friend bool operator>(const TableEntry<V> &te1, const TableEntry<V> &te2){
+			return te1.key > te2.key;
+		}
+		friend bool operator<(const TableEntry<V> &te1, const TableEntry<V> &te2){
+			return te1.key > te2.key;
+		}
 		friend std::ostream& operator<<(std::ostream &out, const TableEntry<V> &te){
 			out<<"{"<<te.key<<"-->"<<te.value <<"}";
 			return out;
